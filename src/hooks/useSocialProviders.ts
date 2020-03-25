b18ecd897d3bae1f634e79d98d0a0a732ff7ba07
ramenskiy <ramenskiy.m@gmail.com>
2020-03-25 16:46:36 +0300

    return if no loginChallenge

--- src/hooks/useSocialProviders.ts
+++ src/hooks/useSocialProviders.ts
@@ -2,13 +2,22 @@ import { useState, useEffect, useContext } from 'react';
 import { getSocialProvidersRequest } from 'api';
 import { SocialProvider } from 'types';
 import { AppContext } from 'App';
+import { getUrlParameter } from 'helpers';
+import { CHALLENGE_KEY } from 'api/const';
 
 export default () => {
   const { setLoading } = useContext(AppContext);
   const [providers, setProviders] = useState<SocialProvider[]>([]);
-  
+  const loginChallenge = getUrlParameter(CHALLENGE_KEY);
+
   const getProviders = async () => {
     setLoading(true);
+
+    if (!loginChallenge) {
+      setProviders([]);
+      setLoading(false);
+      return;
+    }
 
     const response = await getSocialProvidersRequest();
 
