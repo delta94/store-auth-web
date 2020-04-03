b30f9982287b73bb17a118e50d84fd819671111d
ramenskiy <ramenskiy.m@gmail.com>
2020-04-03 12:50:34 +0300

    add SockJS

--- src/hooks/useSocialSignIn.ts
+++ src/hooks/useSocialSignIn.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { setCookie, deleteCookie } from 'helpers';
 import Centrifuge from 'centrifuge';
+import SockJS from 'sockjs-client';
 import { CHALLENGE_KEY, SOCKET_URL } from 'api/const';
 import { getChallenge } from 'api';
 
@@ -12,12 +13,15 @@ type NotificationPayload = {
 export default () => {
   const [status, setStatus] = useState<string>();
   const [redirectUrl, setRedirectUrl] = useState<string>();
-  const centrifuge = new Centrifuge(SOCKET_URL);
+  const centrifuge = new Centrifuge(SOCKET_URL, {
+    sockjs: SockJS,
+  });
+  const loginChallenge = getChallenge();
 
   useEffect(() => {
-    setCookie(CHALLENGE_KEY, getChallenge());
+    setCookie(CHALLENGE_KEY, loginChallenge);
 
-    centrifuge.subscribe('notification', (payload: NotificationPayload) => {
+    centrifuge.subscribe(`launcher#${loginChallenge}`, (payload: NotificationPayload) => {
       const { status, url } = payload;
       setStatus(status);
       setRedirectUrl(url);
