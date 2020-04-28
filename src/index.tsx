956022ead96f33b84e631dfbb1273f172eafaafe
ramenskiy <ramenskiy.m@gmail.com>
2020-04-28 21:31:11 +0300

    unregister sw

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 (window as any).ipc = (window as any).ipc || {
   send: (eventName: string, params: any) => {
@@ -19,4 +19,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
+serviceWorker.unregister();
