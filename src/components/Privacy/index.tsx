71698fd4709c9307fc93f8a7cadbdf954c45b349
Behuamuh <sven1980@list.ru>
2020-06-01 07:17:08 +0300

    forked launcher & web UI

--- src/components/Privacy/index.tsx
+++ src/components/Privacy/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { BlueLink } from 'styles/common';
+import { BlueLink, StyledLink, GreyText } from 'styles/common';
 import { Row, TinyText } from 'styles/primitives';
+import { isLauncher } from 'helpers';
 
 interface Props {
   className?: string;
@@ -12,13 +13,25 @@ const Privacy = (props: Props) => {
   const { className } = props;
   const { t } = useTranslation();
 
+  const webLink = (
+    <StyledLink to="/privacy-policy">
+      <GreyText>
+        {t('privacy-policy')}
+      </GreyText>
+    </StyledLink>
+  );
+
+  const launcherLink = (
+    <TinyText>
+      <BlueLink to="/privacy-policy">
+        {t('privacy-policy')}
+      </BlueLink>
+    </TinyText>
+  );
+
   return (
     <Wrapper className={className}>
-      <TinyText>
-        <BlueLink to="/privacy-policy">
-          {t('privacy-policy')}
-        </BlueLink>
-      </TinyText>
+      {isLauncher ? launcherLink : webLink}
     </Wrapper>
   );
 };
--- src/styles/primitives.tsx
+++ src/styles/primitives.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { PURPLE_500, GRAY_800, ORANGE_500 } from './colors';
+import { isLauncher } from 'helpers';
 
 export const Row = styled.div`
   display: flex;
@@ -39,7 +40,7 @@ export const Button = styled.button<{ color?: string }>`
   background: ${({ disabled, color = PURPLE_500 }) => disabled ? GRAY_800 : color};
   color: white;
   border: 0;
-  border-radius: 4px;
+  border-radius: ${isLauncher ? '4px' : '2px'};
   font-weight: 500;
   font-size: 12px;
   line-height: 150%;
